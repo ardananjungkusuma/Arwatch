c55dfc4a5a8264340bf99563ed7101e3eec0d21b
root <root@DESKTOP-CQ8IGUJ.localdomain>
2019-07-12 09:32:58 +0700

    beta v1.0

--- app/src/main/java/com/blogspot/ardananjungkusuma/arwatch/Checkout.java
+++ app/src/main/java/com/blogspot/ardananjungkusuma/arwatch/Checkout.java
@@ -1,7 +1,9 @@
 package com.blogspot.ardananjungkusuma.arwatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Checkout extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class Checkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+    }
+
+    public void Order_Now(View view) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"ardananjungkusuma@gmail.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "I want to order ... ");
+        i.putExtra(Intent.EXTRA_TEXT, "This is my Address");
+        startActivity(i.createChooser(i, "Choose Email "));
     }
 }
